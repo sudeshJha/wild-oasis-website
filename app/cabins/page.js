4ff31aeab8d635a0122228dde76661bf190a3b05
Sudesh Jha <sudeshjha2005@gmail.com>
2026-08-11 23:26:00 +0530

    loader component

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,20 @@
-export default function Page() {
-  return <h1>Cabins page</h1>;
+import Counter from "../components/Counter";
+
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+  console.log(users);
+
+  return (
+    <div>
+      <h1>Cabins page</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+
+      <Counter users={users} />
+    </div>
+  );
 }
