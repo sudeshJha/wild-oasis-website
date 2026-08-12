aa73c96e8dee9e92633de9e5613eaa95393a2ae8
Sudesh Jha <sudeshjha2005@gmail.com>
2026-08-12 10:34:59 +0530

    header navigation

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Header from "./_components/Header";
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 
@@ -22,14 +23,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+
+        <div className="flex-1 bg-red-500 px-8 py-12">
+          <main className="max-w-7xl  mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
