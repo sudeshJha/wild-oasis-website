c0a3af1be4a5526c2b3da0e1fbcce804625d8f23
Sudesh Jha <sudeshjha2005@gmail.com>
2026-08-15 13:46:08 +0530

    suspense cabins data loading

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,13 @@
-import Counter from "@/app/_components/Counter";
-import CabinCard from "../_components/CabinCard";
-import { getCabins } from "@/public/_lib/data-service";
+import { Suspense } from "react";
+import CabinList from "../_components/CabinList";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default async function Page() {
+export default function Page() {
   // CHANGE
-  const cabins = await getCabins();
 
   return (
     <div>
@@ -24,13 +23,9 @@ export default async function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
