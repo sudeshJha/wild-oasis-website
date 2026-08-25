bbe88c79702e8144a9575f9a61c1f859b6fe8e26
Sudesh Jha <sudeshjha2005@gmail.com>
2026-08-26 02:54:56 +0530

    new guest user on sign in

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,6 @@
 "use server";
 
-import { signIn } from "./auth";
+import { signIn, signOut } from "./auth";
 
 export async function signinAction() {
   await signIn("google", { redirectTo: "/account" });
--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -12,6 +13,25 @@ const authConfig = {
   callbacks: {
     authorized({ auth, request }) {
       return !!auth?.user;
+    },
+
+    async signIn({ user, account, profile }) {
+      try {
+        const guest = await getGuest(user.email);
+
+        if (!guest) {
+          await createGuest({ email: user.email, fullName: user.name });
+        }
+        return true;
+      } catch {
+        return false;
+      }
+    },
+
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     },
   },
 
