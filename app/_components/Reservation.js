d0b999487909a5b9e67b0ac3380c3e5f2db8321e
Sudesh Jha <sudeshjha2005@gmail.com>
2026-08-26 00:38:59 +0530

    next auth middleware

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,12 +1,17 @@
 import { getBookedDatesByCabinId, getSettings } from "@/app/_lib/data-service";
 import DateSelector from "./DateSelector";
 import ReservationForm from "./ReservationForm";
+import { auth } from "../_lib/auth";
+import LoginMessage from "./LoginMessage";
 
 export async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+
+  const session = await auth();
+
   return (
     <div className="grid grid-cols-[1.5fr_1fr] gap-1 border border-primary-800 mt-10 text-accent-400 min-w-[1000px] w-[1300px] mx-auto">
       <DateSelector
@@ -14,7 +19,11 @@ export async function Reservation({ cabin }) {
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
